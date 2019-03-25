ba7d4d46ccc0cfb5b68645f85e0db2e46dee5460
Roman Bulinski <aboandrus@gmail.com>
2019-03-25 18:43:53 +0100

    Implemented code

--- src/main/java/Ox.java
+++ src/main/java/Ox.java
@@ -1,18 +1,24 @@
 public class Ox {
 
     public static boolean getXO (String str) {
-
-
+        str = str.toLowerCase();
         int x = 0;
         int o = 0;
-        char lettere = str.toCharArray();
+        char[] letteres = str.toCharArray();
+        for( char letter : letteres ){
 
+            if(letter == 'x'){
+                x++;
+            }
+            if(letter == 'o'){
+                o++;
+            }
+        }
 
-
-
-
-
+        if(x==o){
+            return true;
+        }
+        return false;
     }
-
 
 }
